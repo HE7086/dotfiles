6c7083290db21a1a86252c90aad1a3c0eb33dcf4
HE7086 <heyi7086@gmail.com>
2021-12-16 11:49:24 +0100

    Shovelling coal into the server...

--- vim/.config/nvim/lua/plugins/lsp/init.lua
+++ vim/.config/nvim/lua/plugins/lsp/init.lua
@@ -15,3 +15,16 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 )
 vim.cmd 'autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()'
 vim.cmd 'autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()'
+
+local map = require('util.keymap').map
+
+map('n', 'gD',     '<CMD>lua vim.lsp.buf.declaration()<CR>')
+map('n', 'gd',     '<CMD>lua vim.lsp.buf.definition()<CR>')
+map('n', 'K',      '<CMD>lua vim.lsp.buf.hover()<CR>')
+map('n', 'gi',     '<CMD>lua vim.lsp.buf.implementation()<CR>')
+map('n', '<C-k>',  '<CMD>lua vim.lsp.buf.signature_help()<CR>')
+map('n', 'gr',     '<CMD>lua vim.lsp.buf.references()<CR>')
+map('n', 'g[',     '<CMD>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n', 'g]',     '<CMD>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n', '<F18>',  '<CMD>lua vim.lsp.buf.rename()<CR>') -- shift F6
+map('n', '<A-CR>', '<CMD>lua vim.lsp.buf.code_action()<CR>')
