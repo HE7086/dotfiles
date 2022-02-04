8a74d9fc5ba5cde5df3ddae86a80a000866450ee
HE7086 <heyi7086@gmail.com>
2022-02-04 01:55:45 +0100

    i think i fixed a bug...

--- plasma/.config/plasma-workspace/env/wayland.sh
+++ plasma/.config/plasma-workspace/env/wayland.sh
@@ -5,15 +5,35 @@ if [[ "$XDG_SESSION_TYPE" = "wayland" ]]; then
     export MOZ_ENABLE_WAYLAND=1
     export QT_QPA_PLATFORM=wayland
 
-    export GTK_IM_MODULE=fcitx
-    export QT_IM_MODULE=fcitx
-    export XMODIFIERS=@im=fcitx
-    export INPUT_METHOD=fcitx
-    export SDL_IM_MODULE=fcitx
-
     setxkbmap -option caps:escape_shifted_capslock
 else
     # fix x11 crashing when relogin from wayland
     unset MOZ_ENABLE_WAYLAND
     unset QT_QPA_PLATFORM
+fi
+
+# general environmental variables
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+export INPUT_METHOD=fcitx
+export SDL_IM_MODULE=fcitx
+
+export SSH_ASKPASS=/usr/bin/ksshaskpass
+
+export GTK_USE_PORTAL=1
+
+# fix vscode delete freeze
+export ELECTRON_TRASH=gio
+
+eval $(gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK
+
+export DEBUGINFOD_URLS="https://debuginfod.archlinux.org/"
+
+if [[ "$HOST" = "HE-workstation" ]]; then
+    # machine specific variables for nvidia-vaapi-driver
+    export MOZ_DISABLE_RDD_SANDBOX=1
+    export MOZ_X11_EGL=1
+    export LIBVA_DRIVER_NAME=nvidia
 fi
