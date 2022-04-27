be36bf0dcd2dc25c9cb53ffc0ca4aef46b1b6219
HE7086 <heyi7086@gmail.com>
2022-04-27 19:58:19 +0200

    Fuck it, YOLO!

--- nvim/.config/nvim/lua/util/path.lua
+++ nvim/.config/nvim/lua/util/path.lua
@@ -1,46 +1,46 @@
 local M = {}
 
 local get_path_separator = function()
-	if vim.fn.has("win32") == 1 or vim.fn.has("win32unix") then
-		return "/"
-	else
-		return "\\"
-	end
+    if vim.fn.has("win32") == 1 or vim.fn.has("win32unix") then
+        return "/"
+    else
+        return "\\"
+    end
 end
 
 M.path_separator = get_path_separator()
 
 M.split = function(path)
-	local pattern = string.format("([^%s]+)", M.path_separator)
-	local result = {}
+    local pattern = string.format("([^%s]+)", M.path_separator)
+    local result = {}
 
-	for str in string.gmatch(path, pattern) do
-		table.insert(result, str)
-	end
+    for str in string.gmatch(path, pattern) do
+        table.insert(result, str)
+    end
 
-	return result
+    return result
 end
 
 M.join = function(...)
-	local args = { ... }
-	if #args == 0 then
-		return ""
-	end
+    local args = { ... }
+    if #args == 0 then
+        return ""
+    end
 
-	local result = {}
+    local result = {}
 
-    if args[1]:sub(1,1) == M.path_separator then
+    if args[1]:sub(1, 1) == M.path_separator then
         -- empty object for abs path
         table.insert(result, '')
     end
 
-	for _, arg in ipairs(args) do
+    for _, arg in ipairs(args) do
         for _, part in ipairs(M.split(arg)) do
             table.insert(result, part)
         end
-	end
+    end
 
-	return table.concat(result, M.path_separator)
+    return table.concat(result, M.path_separator)
 end
 
 M.exists = function(path)
