a6117fad0460c861c10bfef94924afda2a3369be
HE7086 <heyi7086@gmail.com>
2022-10-13 19:23:20 +0200

    Just committing so I can go home

--- PKGBUILD/slack-wayland/slack-wayland.sh
+++ PKGBUILD/slack-wayland/slack-wayland.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "${XDG_SESSION_TYPE}" = "wayland" ]; then
-	exec electron17 /usr/lib/slack/app.asar --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WaylandWindowDecorations --ozone-platform=wayland "$@"
+	exec electron20 /usr/lib/slack/app.asar --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WaylandWindowDecorations --ozone-platform=wayland "$@"
 else
-	exec electron17 /usr/lib/slack/app.asar "$@"
+	exec electron20 /usr/lib/slack/app.asar "$@"
 fi
 
 
