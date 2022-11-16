e23f57c7ed067a1f33cd7fbdb063cc804ed7547d
HE7086 <me@heyi7086.com>
2022-11-16 03:58:40 +0100

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -5,166 +5,136 @@ local check_packer = function()
         vim.fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", "--depth=1", install_path })
         -- run PackerInstall to install plugins
         vim.api.nvim_command("packadd packer.nvim")
-        -- first time will generate a lot of errors, just ignore
         return true
     end
     return false
 end
 local bootstrap = check_packer()
 
-require('packer').startup({function(use)
-    use { 'wbthomason/packer.nvim' }
-    use { 'tpope/vim-surround' }
-    use { 'tpope/vim-repeat' }
-    use { 'tpope/vim-commentary' }
-    use { 'tpope/vim-fugitive' }
-    use { 'tommcdo/vim-exchange' }
-    use { 'terryma/vim-multiple-cursors' }
-    use { 'Konfekt/vim-CtrlXA' }
-    -- use {'rakr/vim-one',
-    --     config = function()
-    --         vim.g.one_allow_italics = 1
-    --         vim.cmd 'colorscheme one'
-    --     end
-    -- }
-    use { 'navarasu/onedark.nvim',
+require("packer").startup({ function(use)
+    use { "wbthomason/packer.nvim" }
+    use { "tpope/vim-surround" }
+    use { "tpope/vim-repeat" }
+    use { "tpope/vim-commentary" }
+    use { "tpope/vim-fugitive" }
+    use { "tommcdo/vim-exchange" }
+    use { "terryma/vim-multiple-cursors" }
+    use { "Konfekt/vim-CtrlXA" }
+
+    use { "rust-lang/rust.vim" }
+    use { "HE7086/objdump.vim" }
+    use { "killphi/vim-ebnf" }
+    use { "LnL7/vim-nix" }
+
+    use { "navarasu/onedark.nvim",
         config = function()
-            require('onedark').setup {
-                highlights = {
-                    PmenuSel = { bg = "#282C34", fg = "NONE" },
-                    Pmenu = { fg = "#C5CDD9", bg = "#22252A" }
-                }
-            }
-            require('onedark').load()
-            -- vim.api.nvim_command("colorscheme onedark")
+            pcall(require, "plugins.onedark")
         end
     }
+    use { "cormacrelf/vim-colors-github" }
+    use { "kyazdani42/nvim-web-devicons" }
 
-    use { 'cormacrelf/vim-colors-github',
-        -- config = function()
-        --     vim.cmd("colorscheme github")
-        --     vim.cmd("set bg=light")
-        -- end
-    }
-
-    -- use {'altercation/vim-colors-solarized',
-    --     config = function()
-    --         vim.cmd("colorscheme solarized")
-    --         vim.cmd("set bg=light")
-    --     end
-    -- }
-    use { 'kyazdani42/nvim-web-devicons' }
-
-    -- use {'glepnir/galaxyline.nvim',
-    --     branch = 'main',
-    --     config = function() require('plugins.galaxyline') end,
-    --     requires = {'kyazdani42/nvim-web-devicons'}
-    -- }
-
-    use { 'skywind3000/asyncrun.vim' }
-    use { 'Yggdroot/LeaderF',
+    use { "skywind3000/asyncrun.vim" }
+    use { "Yggdroot/LeaderF",
         run = function()
-            vim.api.nvim_command('LeaderfInstallCExtension')
+            vim.api.nvim_command("LeaderfInstallCExtension")
         end,
         config = function()
-            vim.g.Lf_CacheDirectory = vim.fn.stdpath("cache") .. '/LeaderF'
-            require('plugins.leaderf')
+            vim.g.Lf_CacheDirectory = vim.fn.stdpath("cache") .. "/LeaderF"
+            pcall(require, "plugins.leaderf")
         end
     }
 
-    use { 'mhinz/vim-startify',
+    use { "mhinz/vim-startify",
         config = function()
-            require('plugins.startify')
+            pcall(require, "plugins.startify")
         end
     }
 
-    use { 'lewis6991/gitsigns.nvim',
+    use { "lewis6991/gitsigns.nvim",
         requires = {
-            { 'nvim-lua/plenary.nvim' },
+            { "nvim-lua/plenary.nvim" },
         },
         config = function()
-            require('plugins.gitsigns')
+            pcall(require, "plugins.gitsigns")
         end
-
     }
 
-    use { 'iamcco/markdown-preview.nvim',
+    use { "iamcco/markdown-preview.nvim",
         run = function()
-            vim.fn['mkdp#util#install']()
+            vim.fn["mkdp#util#install"]()
         end,
-        ft = { 'markdown' },
+        ft = { "markdown" },
         cmd = {
-            'MarkdownPreview',
-            'MarkdownPreviewToggle'
+            "MarkdownPreview",
+            "MarkdownPreviewToggle"
         }
     }
 
-    use { 'nvim-telescope/telescope.nvim',
+    use { "nvim-telescope/telescope.nvim",
         requires = {
-            { 'nvim-telescope/telescope-file-browser.nvim' },
-            { 'nvim-lua/popup.nvim' },
-            { 'nvim-lua/plenary.nvim' },
+            { "nvim-telescope/telescope-file-browser.nvim" },
+            { "nvim-lua/popup.nvim" },
+            { "nvim-lua/plenary.nvim" },
         },
         config = function()
-            require('plugins.telescope')
+            pcall(require, "plugins.telescope")
         end
     }
 
-    use { 'mhartington/formatter.nvim',
+    use { "mhartington/formatter.nvim",
         config = function()
-            require('plugins.formatter')
+            pcall(require, "plugins.formatter")
         end
     }
 
-    use { 'rust-lang/rust.vim' }
-    use { 'HE7086/objdump.vim' }
-
-    -- use {'rktjmp/lush.nvim'}
-    use { 'killphi/vim-ebnf' }
-    use { 'LnL7/vim-nix' }
-
-    use { 'lukas-reineke/indent-blankline.nvim',
+    use { "lukas-reineke/indent-blankline.nvim",
         config = function()
-            require("indent_blankline").setup {
-                space_char_blankline = " ",
-                show_current_context = true,
-                show_current_context_start = true,
-            }
+            pcall(require, "plugins.indent-blankline")
         end
     }
-
-    -- use {'SirVer/ultisnips',
-    --     config = function() require('plugins.ultisnips') end,
-    --     requires = {{'honza/vim-snippets'}}
-    -- }
 
     ----- lsp plugins -----
-    use { 'nvim-treesitter/nvim-treesitter',
-        -- run = ':TSUpdate',
+    use { "nvim-treesitter/nvim-treesitter",
+        -- run = ":TSUpdate",
         config = function()
-            require('plugins.nvim-treesitter')
+            pcall(require, "plugins.nvim-treesitter")
         end
     }
-    use { 'neovim/nvim-lspconfig',
+    use { "neovim/nvim-lspconfig",
         config = function()
-            require('plugins.lsp')
+            pcall(require, "plugins.lsp")
         end
     }
-    use { 'hrsh7th/nvim-cmp',
+    use { "hrsh7th/nvim-cmp",
         config = function()
-            require('plugins.cmp')
+            pcall(require, "plugins.cmp")
         end,
         requires = {
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'hrsh7th/cmp-cmdline' },
-            -- {'SirVer/ultisnips'},
-            -- {'quangnguyen30192/cmp-nvim-ultisnips'}
-            { 'hrsh7th/cmp-vsnip' },
-            { 'hrsh7th/vim-vsnip' },
-            { 'onsails/lspkind.nvim' },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "hrsh7th/cmp-cmdline" },
+            { "hrsh7th/cmp-vsnip" },
+            { "hrsh7th/vim-vsnip" },
+            { "onsails/lspkind.nvim" },
         }
+    }
+    use { "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {}
+        end
+    }
+    use { "williamboman/mason.nvim",
+        requires = {
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason-lspconfig.nvim" },
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup()
+        end
     }
 
     use { vim.fn.stdpath("config") .. "/myplugins" }
@@ -173,6 +143,7 @@ require('packer').startup({function(use)
         require("packer").sync()
     end
 end,
-config = {
-    display = { open_fn = require('packer.util').float },
-}})
+    config = {
+        display = { open_fn = require("packer.util").float },
+        profile = { enable = true },
+    } })
