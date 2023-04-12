1b9a255cd7adadec5c323605df0d851d1606a73a
HE7086 <me@heyi7086.com>
2023-04-12 17:05:11 +0200

    ﻿ - Temporary commit.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -71,10 +71,10 @@ local plugins = {
             { "nvim-telescope/telescope-file-browser.nvim" },
             { "nvim-lua/popup.nvim" },
             { "nvim-lua/plenary.nvim" },
-            { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
         },
         config = function()
-            pcall(require, "plugins.telescope")
+            require("plugins.telescope")
         end
     },
 
