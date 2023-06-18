999716a64582fa9e6e86009cec8cb6821d7c2e13
HE7086 <me@heyi7086.com>
2023-06-18 02:05:40 +0200

    Commit committed....

--- PKGBUILD/slack-wayland/slack-wayland.sh
+++ PKGBUILD/slack-wayland/slack-wayland.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "${XDG_SESSION_TYPE}" = "wayland" ]; then
-	exec electron21 /usr/lib/slack/app.asar --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WaylandWindowDecorations --ozone-platform=wayland --enable-wayland-ime "$@"
+	exec electron /usr/lib/slack/app.asar --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WaylandWindowDecorations --ozone-platform=wayland --enable-wayland-ime "$@"
 else
-	exec electron21 /usr/lib/slack/app.asar "$@"
+	exec electron /usr/lib/slack/app.asar "$@"
 fi
 
 
