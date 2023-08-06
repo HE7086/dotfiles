c25add47a1143d157de7573477d3fc0a375c1f7d
HE7086 <me@heyi7086.com>
2023-08-06 19:33:59 +0200

    formatted all

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -73,7 +73,7 @@ local plugins = {
             { "nvim-telescope/telescope-file-browser.nvim" },
             { "nvim-lua/popup.nvim" },
             { "nvim-lua/plenary.nvim" },
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         },
         config = function()
             require("plugins.telescope")
