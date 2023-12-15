b8fb69b2a1f72890298b67fb8d6b05fb1e69d991
HE7086 <me@heyi7086.com>
2023-12-15 12:59:48 +0100

    diaaaaaazeeeeeeeeeepam

--- PKGBUILD/slack-wayland/slack-wayland.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "${XDG_SESSION_TYPE}" = "wayland" ]; then
-	exec electron /usr/lib/slack/app.asar --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WaylandWindowDecorations --ozone-platform=wayland --enable-wayland-ime "$@"
-else
-	exec electron /usr/lib/slack/app.asar "$@"
-fi
-
-
