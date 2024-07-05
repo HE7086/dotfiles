a6c6631338ba8bbc10d9b4ed007acec1a6f28f6a
HE7086 <me@heyi7086.com>
2024-07-05 20:39:04 +0200

    I must sleep... it's working... in just three hours...

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -31,21 +31,7 @@ return {
       require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts)
       local npairs = require("nvim-autopairs")
       local Rule = require("nvim-autopairs.rule")
-      local cond = require("nvim-autopairs.conds")
       npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(cond.not_before_regex("xxx", 3))
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
         -- disable for .vim files, but it work for another filetypes
         Rule("a", "a", "-vim")
       )
