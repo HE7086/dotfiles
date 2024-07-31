51888d3c6413e8d49e56dd8bf36cad5311fcb1d6
HE7086 <me@heyi7086.com>
2024-07-31 14:25:46 +0200

    remove redundant command

--- nvim/.config/nvim/lua/custom.lua
+++ nvim/.config/nvim/lua/custom.lua
@@ -1,5 +1,1 @@
 vim.api.nvim_create_user_command("W", "w", {})
-vim.api.nvim_create_user_command("LU", function()
-  require("astrocore").update_packages()
-  vim.api.nvim_command("TSUpdate")
-end, {})
