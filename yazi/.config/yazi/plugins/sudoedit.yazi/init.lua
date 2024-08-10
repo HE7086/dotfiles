246e4bd36d12fbe201737f7e359cd02e9e29cc96
HE7086 <me@heyi7086.com>
2024-08-10 23:10:44 +0200

    features

--- yazi/.config/yazi/plugins/sudoedit.yazi/init.lua
+++ yazi/.config/yazi/plugins/sudoedit.yazi/init.lua
@@ -1,9 +1,14 @@
 return {
 	entry = function(_, _)
-		ya.manager_emit("shell", {
-			'sudoedit "$@"',
-			block = true,
-			confirm = true,
-		})
+		local h = cx.active.current.hovered
+		if h and h.cha.is_dir then
+			ya.manager_emit("enter", {})
+		else 
+			ya.manager_emit("shell", {
+				'sudoedit "$@"',
+				block = true,
+				confirm = true,
+			})
+		end
 	end
 }
