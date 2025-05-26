6dbbc0f738c8656249363a90572177da1ed40db1
HE7086 <me@heyi7086.com>
2025-05-27 00:59:44 +0200

    I'll explain this when I'm sober .. or revert it

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -23,6 +23,6 @@ return {
   },
 
   -- https://github.com/LazyVim/LazyVim/issues/6039
-  { "mason-org/mason.nvim", version = "1.11.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+  -- { "mason-org/mason.nvim", version = "1.11.0" },
+  -- { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 }
